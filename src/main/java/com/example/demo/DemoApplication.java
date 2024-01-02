c433a75ef2d1686397eeb7f67aedf20b2666eab0
Tamilarasan Murugan <tamilmurugan2602@gmail.com>
2024-01-02 19:09:20 +0530

    tamil

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,21 @@
 package com.example.demo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+//import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
+//@RestController
 @EnableAutoConfiguration
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String home()
-	{
-		return "Hello Kamal";
-	}
-
+	// @RequestMapping("/")
+	// String home()
+	// {
+	// 	return "Hello Kamal";
+	// }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
